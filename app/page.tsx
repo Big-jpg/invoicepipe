31eaccfc8521e410d1585e87e8f6add8bf86dbc2
Ross Farrell <56429528+Big-jpg@users.noreply.github.com>
2025-11-19 18:08:19 +0800

    Rename AltLandingPage to LandingPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+// app/page.tsx
 "use client";
 
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
-export default function AltLandingPage() {
+export default function LandingPage() {
     return (
         <main className="min-h-screen bg-background text-foreground flex flex-col">
             {/* Top Bar */}
